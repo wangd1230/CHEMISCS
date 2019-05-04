3d6c12bb54e94590b407a65d606fb359aa839858
Your Name <you@example.com>
2019-05-04 16:54:27 +0800

    third

---
+++ sw.js
@@ -0,0 +1,23 @@
+const staticAssets = [
+    './',
+    './style.css',
+    './app.js'
+];
+
+
+
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open('final-static');
+    cache.addAll(staticAssets); 
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    event.respondWith(cacheFirst(req));
+});
+
+async function cacheFirst(req) {
+    const cachedResponse = await caches.match(req);
+    return cachedResponse || fetch(req);
+}
