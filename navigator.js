e7ccaeabe3427fbda002efaeb5511c8281fa832f
Your Name <you@example.com>
2019-05-05 22:19:35 +0800

    11

--- navigator.js
+++ navigator.js
@@ -2,12 +2,16 @@ var home = document.querySelector('.home');
 var me = document.querySelector('.me');
 var secondPage = document.querySelector('.secondPage');
 var firstPage = document.querySelector('.firstPage');
+var main = document.querySelector('.main');
 
 me.addEventListener('click',function(){
 	firstPage.style.display = 'none';
 	secondPage.style.display = 'block';
+	main.style.backgroundImage = "url('img/bg.png')"
+
 })
 home.addEventListener('click',function(){
 	secondPage.style.display = 'none';
-	firstPage.style.display = 'black';
+	firstPage.style.display = 'block';
+	main.style.backgroundImage ='none';
 })
