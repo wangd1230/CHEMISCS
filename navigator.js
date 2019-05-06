a65d6f768e3a1c76f81c288ffb80deebee4a0059
Your Name <you@example.com>
2019-05-06 10:14:44 +0800

    13

--- navigator.js
+++ navigator.js
@@ -3,6 +3,7 @@ var me = document.querySelector('.me');
 var secondPage = document.querySelector('.secondPage');
 var firstPage = document.querySelector('.firstPage');
 var main = document.querySelector('.main');
+var book1 = document.querySelector('.book1');
 
 me.addEventListener('click',function(){
 	firstPage.style.display = 'none';
@@ -14,4 +15,7 @@ home.addEventListener('click',function(){
 	secondPage.style.display = 'none';
 	firstPage.style.display = 'block';
 	main.style.backgroundImage ='none';
+})
+book1.addEventListener('click',function(){
+	window.location.href = 'pages/newton.html';
 })
